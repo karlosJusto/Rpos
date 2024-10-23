8f6cf4532d60db7d2e4af820eb6b5154415c45a6
Carlos Justo <justo.karlos@gmail.com>
2024-10-23 20:40:16 +0200

    primer cambio

--- src/components/pedidos/Ticket.jsx
+++ src/components/pedidos/Ticket.jsx
@@ -70,7 +70,7 @@ const Ticket = () => {
               <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
                   <img src={patatassin} alt="patatas" className='w-12'/>
                   <span className="ms-3 whitespace-nowrap"><span>1</span> x </span>
-                  <span className="flex-1 ms-3 whitespace-nowrap">Patatas Fritas</span>
+                  <span className="flex-1 ms-3 whitespace-nowrap">Patatas Fritass</span>
                  
                   <span className="inline-flex items-center justify-center px-2 py-0.5 ms-3 text-md font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">88.50 <span>€</span></span>
                   </a>
