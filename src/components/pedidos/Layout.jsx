cd2cf65eb7b03b445365fc3d765d5bc8e420d65d
Carlos Justo <justo.karlos@gmail.com>
2024-11-10 18:41:02 +0100

    conexion db

--- src/components/pedidos/Layout.jsx
+++ src/components/pedidos/Layout.jsx
@@ -32,7 +32,7 @@ const Layout = () => {
               </div>
 
 
-              <div className=" bg-gradient-to-r from-gray-100 to-gray-300 text-center  w-[63%] rounded-xl overflow-y-auto mt-2 mb-36">
+              <div className=" bg-gradient-to-r from-gray-100 to-gray-300 text-center  w-[63%] rounded-xl overflow-y-auto mt-2 mb-44 ">
                 
                       <div className="grid grid-cols-4  ">
 
@@ -41,6 +41,7 @@ const Layout = () => {
                       
                         
                       </div>
+                      
               </div>
 
                 <div className=" w-[29%] ms-2 rounded-2xl mt-2 ">
